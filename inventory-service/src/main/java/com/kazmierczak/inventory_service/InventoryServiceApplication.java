ce7e0a26b3532e36f1188533a74827a1622e95a1
Patryk Kaźmierczak <patryk.kazmierczak@gazeta.pl>
2024-07-14 23:37:06 +0200

    inventory service added

--- inventory-service/src/main/java/com/kazmierczak/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/kazmierczak/inventory_service/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.kazmierczak.inventory_service;
 
+import com.kazmierczak.inventory_service.model.Inventory;
+import com.kazmierczak.inventory_service.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,19 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("Corolla");
+			inventory1.setQuantity(2);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("Yaris");
+			inventory2.setQuantity(0);
+
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory2);
+		};
+	}
 }
--- inventory-service/src/main/java/com/kazmierczak/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/kazmierczak/inventory_service/service/InventoryService.java
@@ -1,2 +1,17 @@
-package com.kazmierczak.inventory_service.service;public class InventoryService {
+package com.kazmierczak.inventory_service.service;
+
+import com.kazmierczak.inventory_service.repository.InventoryRepository;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class InventoryService {
+    private final InventoryRepository inventoryRepository;
+
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
 }
