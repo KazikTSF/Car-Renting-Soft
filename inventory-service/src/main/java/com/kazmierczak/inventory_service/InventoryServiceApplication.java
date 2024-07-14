522f827c0449cb390343081ff0e1698329ecb8d1
Patryk Kaźmierczak <patryk.kazmierczak@gazeta.pl>
2024-07-15 00:00:59 +0200

    inventory service testing

--- inventory-service/src/main/java/com/kazmierczak/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/kazmierczak/inventory_service/InventoryServiceApplication.java
@@ -14,19 +14,4 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
-		return args -> {
-			Inventory inventory1 = new Inventory();
-			inventory1.setSkuCode("Corolla");
-			inventory1.setQuantity(2);
-
-			Inventory inventory2 = new Inventory();
-			inventory2.setSkuCode("Yaris");
-			inventory2.setQuantity(0);
-
-			inventoryRepository.save(inventory1);
-			inventoryRepository.save(inventory2);
-		};
-	}
 }
--- inventory-service/src/test/java/com/kazmierczak/inventory_service/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/kazmierczak/inventory_service/InventoryServiceApplicationTests.java
@@ -1,13 +1,71 @@
 package com.kazmierczak.inventory_service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kazmierczak.inventory_service.model.Inventory;
+import com.kazmierczak.inventory_service.repository.InventoryRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MySQLContainer;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class InventoryServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	static MySQLContainer mysql = new MySQLContainer("mysql:latest")
+			.withDatabaseName("order-service")
+			.withUsername("root")
+			.withPassword("root");
+	static {
+		mysql.start();
 	}
-
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private InventoryRepository inventoryRepository;
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.datasource.url", mysql::getJdbcUrl);
+		dynamicPropertyRegistry.add("spring.datasource.username", mysql::getUsername);
+		dynamicPropertyRegistry.add("spring.datasource.password", mysql::getPassword);
+	}
+	@AfterEach
+	public void cleanup() {
+		inventoryRepository.deleteAll();
+	}
+	@Test
+	void isInStock() throws Exception {
+		Inventory inv = Inventory.builder()
+				.skuCode("Corolla2022")
+				.quantity(1)
+				.build();
+		inventoryRepository.save(inv);
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/inventory/Corolla2022"))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("$").value(true));
+	}
+	@Test
+	void isNotInStock() throws Exception {
+		Inventory inv = Inventory.builder()
+				.skuCode("Yaris2023")
+				.quantity(1)
+				.build();
+		inventoryRepository.save(inv);
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/inventory/Corolla2022"))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("$").value(false));
+	}
 }
