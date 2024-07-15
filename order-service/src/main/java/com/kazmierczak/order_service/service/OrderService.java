bd5ce144afa34a9bbda4f76d4f1c05835a4d8648
Patryk Kaźmierczak <patryk.kazmierczak@gazeta.pl>
2024-07-16 01:12:22 +0200

    kafka message

--- order-service/src/main/java/com/kazmierczak/order_service/service/OrderService.java
+++ order-service/src/main/java/com/kazmierczak/order_service/service/OrderService.java
@@ -3,10 +3,12 @@ package com.kazmierczak.order_service.service;
 import com.kazmierczak.order_service.dto.InventoryResponse;
 import com.kazmierczak.order_service.dto.OrderCarDto;
 import com.kazmierczak.order_service.dto.OrderRequest;
+import com.kazmierczak.order_service.event.OrderPlacedEvent;
 import com.kazmierczak.order_service.model.Order;
 import com.kazmierczak.order_service.model.OrderCar;
 import com.kazmierczak.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -19,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
-
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -27,13 +29,14 @@ public class OrderService {
         order.setOrderCar(orderCar);
 
         InventoryResponse result = webClientBuilder.build().get()
-                .uri("http://localhost:8082/api/inventory/" + orderCar.getSkuCode())
+                .uri("http://inventory-service/api/inventory/" + orderCar.getSkuCode())
                 .retrieve()
                 .bodyToMono(InventoryResponse.class)
                 .block();
 
         if(result.isInStock()) {
             orderRepository.save(order);
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
         }
         else
             throw new IllegalArgumentException("Product not in stock");
